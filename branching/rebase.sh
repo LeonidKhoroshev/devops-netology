d11365942358b77d57e0ac1a5d2266a8bda3ea54
Leonid Khoroshev <khoroshevlv@gmail.com>
2023-09-25 17:25:48 +0300

    git-rebase 1

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # display command line options
-
 count=1
 for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
+    echo "Parameter: $param"
     count=$(( $count + 1 ))
 done
 
